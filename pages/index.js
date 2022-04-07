2aa840a6a6005cae2368b695e7c943aae3a69258
seansanchez951 <69922895+seansanchez951@users.noreply.github.com>
2022-04-06 22:25:58 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,7 @@ export default function Home() {
 	
 	<p className="alpharelease">
           <h2>Alpha Release Links</h2>
+	  <p><a href="https://docs.google.com/document/d/e/2PACX-1vSAkBP-bzRK1i7W22prsx0DKBM67qvtTrPYBfyejOUdwqOC5CypkuM5adsRQjNWi_q9Wsj3Q-lDYyB9/pub">Alpha Release Report</a></p>
 	  <p><a href="https://vizhub.com/seansanchez951/c1d1816b4d09400484c2777ba8e96655">Lollipop Chart in d3: Best Year in Music</a></p>
 	  <p><a href="https://vizhub.com/seansanchez951/96a5c03ffb7f470fbc1134ae0af2866c">Bubble Chart in d3: Music Genres Over Time</a></p>
 	  <p><a href="https://photos.app.goo.gl/tFVSeVNfAdS63QXk6">Treemap in RawGraphs: Average Scores Per Genre</a></p>
